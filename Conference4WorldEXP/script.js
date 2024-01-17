ecf0aae6bb9df57bdcdda045fc44f55ef1afdb4f
evesalome <153929706+evesalome@users.noreply.github.com>
2024-01-17 14:44:00 +0100

    Update script.js

--- Conference4WorldEXP/script.js
+++ Conference4WorldEXP/script.js
@@ -36,4 +36,49 @@ var acc= document.getElementsByClassName("question");
         }
 
 
+// Script pour le forum 
+
+var acc= document.getElementsByClassName("question");
+        var i;
+        for(i=0; i < acc.length; i++){
+            acc[i].addEventListener("click", function(){
+                this.classList.toggle("active");
+                this.parentElement.classList.toggle("active");
+                var reponse = this.nextElementSibling;
+
+                if(reponse.style.display === "block"){
+                    reponse.style.display = "none";
+                }
+                else{
+                    reponse.style.display = "block";
+                }
+            });
+        }
+
+
+// barre recherche 
+  function search() {
+    var userInput = document.getElementById("searchInput").value.trim();
+
+    if (userInput === "") {
+        document.getElementById("resultMessage").innerHTML = "Veuillez écrire votre requête.";
+        return;
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var result = xhr.responseText;
+            if (result === "NoResult") {
+                document.getElementById("resultMessage").innerHTML = "Aucun résultat.";
+            } else {
+                document.getElementById("resultMessage").innerHTML = "Résultat : " + result;
+            }
+        }
+    };
+
+    xhr.open("GET", "search.php?query=" + userInput, true);
+    xhr.send();
+}
+
         
