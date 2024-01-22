5edd655b2dfc7fe146e30e70b9cfc9282314a750
Quentam <154077469+Quentam@users.noreply.github.com>
2024-01-23 00:15:37 +0100

    Update script.js
    Calendrier

--- G4D-bonneversion/Conference4WorldEXP/script.js
+++ G4D-bonneversion/Conference4WorldEXP/script.js
@@ -44,3 +44,148 @@ window.addEventListener('DOMContentLoaded', (event) => {
         });
     }
 });
+
+
+
+
+
+
+
+//Calendrier :
+
+// Mise en forme du calendrier
+let Cases = document.getElementsByClassName('case')
+
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth() + 1;
+let day = date.getDate();
+
+const monthName = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"]; 
+
+const UP_MONTH = "upMonth"
+
+const DOWN_MONTH = "downMonth"
+
+function CALENDRIER_REDUCER(action) {
+    switch(action) {
+        case UP_MONTH:
+            if (month < 12) month++
+            else {
+                year++
+                month = 1
+            }
+            break;
+        case DOWN_MONTH:
+            if (month > 1) month--
+            else {
+                year--
+                month = 12
+            }
+            break;
+        default:
+            break;
+    }
+    calendrier(year, month)
+}
+
+ 
+document.getElementById("avant").onclick = function() {
+    CALENDRIER_REDUCER(DOWN_MONTH)
+    console.log(month)
+}
+
+document.getElementById("après").onclick = function() {
+    CALENDRIER_REDUCER(UP_MONTH)
+    console.log(month)
+}
+
+
+function calendrier (year, month) {
+    const monthNb = month + 12 * (year - 2020)
+
+    let cld = [{dayStart: 2, length: 31, year: 2020, month: "janvier"}]
+
+    for (let i = 0; i < monthNb - 1; i++) {
+        let yearSimulée = 2020 + Math.floor(i / 12)
+        const monthsSimulélongueur = [31, getFévrierLength(yearSimulée), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        let monthSimuléIndex = (i + 1) - (yearSimulée - 2020) * 12
+        cld[i + 1] = {
+            dayStart: (cld[i].dayStart + monthsSimulélongueur[monthSimuléIndex - 1]) % 7,
+            length: monthsSimulélongueur[monthSimuléIndex],
+            year: 2020 + Math.floor((i + 1) / 12),
+            month: monthName[monthSimuléIndex]
+        }
+
+        if (cld[i + 1].month === undefined) {
+            cld[i + 1].month = "janvier"
+            cld[i + 1].length = 31
+        }
+    }
+
+    for (let i = 0; i < Cases.length; i++) {
+        Cases[i].innerText = ""
+    }
+
+    for (let i = 0; i < cld[cld.length - 1].length; i++) {
+        Cases[i + cld[cld.length - 1].dayStart].innerText = i + 1
+        
+    }
+
+
+    document.getElementById("calT").innerText = cld[cld.length - 1].month.toLocaleUpperCase() + " " + cld[cld.length - 1].year
+
+}
+
+calendrier(year, month)
+
+ 
+function getFévrierLength(year) {
+    if (year % 4 == 0) return 29
+    else return 28
+}
+
+
+
+
+ document.addEventListener('DOMContentLoaded', function () {
+    const cases = document.querySelectorAll('.case');
+
+    cases.forEach(function (caseElement) {
+        caseElement.addEventListener('click', function () {
+        // Variable globale pour stocker le contenu du modal
+        let modalContentzzz = '';
+            // Vous pouvez personnaliser le contenu du modal ici
+            const modalContent = caseElement.textContent + '/' + month + '/' + year;
+            if (caseElement.textContent == "") {
+                modalContent = ""
+            }
+         // Ajouter le nouveau contenu au contenu existant
+         if (phpday === modalContent) {
+            modalContentzzz += modalContent + '<br>' + phpContent;
+         }
+        else {
+            modalContentzzz += modalContent
+        }
+
+            // Mettre à jour le contenu du modal
+            modal = document.getElementById('modal');
+            modal.innerHTML = modalContentzzz;
+
+            // Affichage du modal
+            document.getElementById('overlay').style.display = 'flex';
+
+            
+            overlay = document.getElementById("overlay")
+
+            // Faire disparaitre le modal
+            window.onclick = function(event) {
+                if (event.target === overlay) {
+                    overlay.style.display = "none";
+                }
+            }
+        });
+    });
+});
+
+
